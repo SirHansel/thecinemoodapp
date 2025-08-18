9f4d980a2126d8c078c8f319d14f82ebdefd9611
SirHansel <HmHeilmann@gmail.com>
2025-08-18 08:19:04 -0700

    Update tmdbApi.js

--- src/tmdbApi.js
+++ src/tmdbApi.js
@@ -1,1 +1,10 @@
+const TMDB_API_KEY = 'your-actual-api-key-here';
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
+export const fetchMoviesByGenre = async (genreId) => {
+  const response = await fetch(
+    `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${genreId}&sort_by=popularity.desc&page=1`
+  );
+  const data = await response.json();
+  return data.results;
+};

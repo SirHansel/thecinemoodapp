ba25cb0de34f667ab62b9f734dface8a094a1319
SirHansel <HmHeilmann@gmail.com>
2025-10-13 12:26:27 -0700

    Update App.js
    }; 423

--- src/App.js
+++ src/App.js
@@ -420,7 +420,7 @@ const allowedGenres = Object.values(TMDB_GENRES).filter(genreId => !excludedGenr
         movies = await fetchMoviesByGenre(secondGenre, true);
       }
     }
-  }
+  };
   
   return {
     movies: movies,

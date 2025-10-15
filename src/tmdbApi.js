1ecbe8f4917dd87fa71bee5376ecfb92f55e96a6
SirHansel <HmHeilmann@gmail.com>
2025-10-15 09:08:24 -0700

    Update tmdbApi.js
    updated the function for the hybridization rating method

--- src/tmdbApi.js
+++ src/tmdbApi.js
@@ -5,9 +5,14 @@ export const fetchMoviesByGenre = async (genreId, allowForeign = false) => {
   const API_KEY = TMDB_API_KEY;
   const languageParam = allowForeign ? '' : '&with_original_language=en';
   
+  // Randomization for variety
+  const randomPage = Math.floor(Math.random() * 3) + 1;
+  const sortOptions = ['vote_average.desc', 'release_date.desc', 'popularity.desc'];
+  const randomSort = sortOptions[Math.floor(Math.random() * sortOptions.length)];
+  
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&sort_by=vote_average.desc&vote_count.gte=100${languageParam}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&sort_by=${randomSort}&vote_count.gte=50&vote_average.gte=6.0&certification_country=US&certification.lte=R&page=${randomPage}${languageParam}`
     );
     const data = await response.json();
     return data.results;
